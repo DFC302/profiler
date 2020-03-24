99e4300d6a1447a7f8d664205c075949356784b9
Matthew Greer <pydev302@gmail.com>
2020-03-24 13:39:29 -0400

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -81,6 +81,11 @@ def arguments():
 		help="Version information.",
 		action="store_true")
 	
+	# if not arguments are given, print usage message
+	if len(sys.argv[1:]) == 0:
+		parser.print_help()
+		parser.exit()
+	
 	args = parser.parse_args()
 
 	return args
